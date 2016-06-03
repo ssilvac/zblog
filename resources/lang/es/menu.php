9adcf970c68788c7e2cdfc8d872c803e1a9697a8
Sebastián Silva <sebasilvac88@gmail.com>
2016-06-02 23:58:13 -0400

    Español

---
+++ resources/lang/es/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	/*
+		    |--------------------------------------------------------------------------
+		    | Menu Language Lines
+		    |--------------------------------------------------------------------------
+		    |
+	*/
+
+	'text_logo' => 'ZBlog',
+	'login' => 'Inicia sesión',
+	'logout' => 'Cierra sesión',
+	'register' => 'Registrate',
+	'home' => 'Raiz',
+	'account' => 'Cuenta',
+	'publish' => 'Publicar',
+	'admin_posts' => 'Posts' 
+
+];
